176382acd66e32c64c312a32562ae5ae51bc27ac
PrincessMaggy <edoziemagdalene@gmail.com>
2021-10-11 10:23:10 +0100

    My portfolio

---
+++ js/app.js
@@ -0,0 +1,153 @@
+$(function() {
+    'use strict';
+
+    // Back to top smooth scroll
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+    // Scroll to top animated button
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-up').fadeIn();
+        } else {
+            $('.scroll-up').fadeOut();
+        }
+    });
+
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+
+    // Script for Portfolio Section
+
+    $(document).ready(function() {
+
+        // Slider top 
+
+
+        $('#myCarousel-one').carousel({
+            interval: 9000, //changes the speed
+            keyboard: false,
+        })
+
+
+    });
+
+    /* Animated Titles of Sections*/
+
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    $(window).scroll(function() {
+        $('.section-heading, .section-subheading').each(function() {
+            if (isScrolledIntoView(this) === true) {
+                $(this).addClass('animated fadeInUp')
+            }
+        });
+
+
+    });
+
+
+    /*  Isotope Filter */
+
+    (function() {
+        var winDow = $(window);
+        var $container = $('.portfolio-items');
+        var $filter = $('.filter');
+
+        try {
+            $container.imagesLoaded(function() {
+                $container.show();
+                $container.isotope({
+                    filter: '*',
+                    layoutMode: 'masonry',
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear'
+                    }
+                });
+            });
+        } catch (err) {}
+
+        winDow.bind('resize', function() {
+            var selector = $filter.find('a.active').attr('data-filter');
+
+            try {
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false,
+                    }
+                });
+            } catch (err) {}
+            return false;
+        });
+
+        $filter.find('a').click(function() {
+            var selector = $(this).attr('data-filter');
+
+            try {
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false,
+                    }
+                });
+            } catch (err) {
+
+            }
+            return false;
+        });
+
+
+    }());
+
+
+    /* Header shrink function*/
+
+    $(function() {
+        var shrinkHeader = 350;
+        $(window).scroll(function() {
+            var scroll = getCurrentScroll();
+            if (scroll >= shrinkHeader) {
+                $('.navbar').addClass('navbar-shrink');
+            } else {
+                $('.navbar').removeClass('navbar-shrink');
+            }
+        });
+
+        function getCurrentScroll() {
+            return window.pageYOffset || document.documentElement.scrollTop;
+        }
+    });
+
+
+});
